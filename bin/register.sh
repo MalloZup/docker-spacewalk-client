16538669f70b8646bf698359b4b6dbfab675dfe7
Dario Maiocchi <MalloZup@users.noreply.github.com>
2017-05-11 22:20:52 +0200

    update register.sh for suse-mgr

--- bin/register.sh
+++ bin/register.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
-# author: Pavel Studenik <pstudeni@redhat.com>
 
-# register.sh <username> <password> <server>
+# authors: Pavel Studenik <pstudeni@redhat.com>
+#          Dario Maiocchi <dmaiocchi@suse.com
+
+# .register.sh <server>
 
 function show_log {
    count_log_2=$( cat /var/log/up2date | wc -l )
@@ -9,8 +11,8 @@ function show_log {
    tail -n $num /var/log/up2date
 }
 
-RHN_USER=${1:-$RHN_USER}
-RHN_PASS=${2:-$RHN_PASS}
+RHN_USER=admin
+RHN_PASS=admin
 RHN_SERVER=${3:-$RHN_SERVER}
 
 wget https://$RHN_SERVER/pub/RHN-ORG-TRUSTED-SSL-CERT -O /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT --no-check-certificate
@@ -18,9 +20,11 @@ wget https://$RHN_SERVER/pub/RHN-ORG-TRUSTED-SSL-CERT -O /usr/share/rhn/RHN-ORG-
 touch /var/log/up2date
 count_log=$( cat /var/log/up2date | wc -l  )
 
+# register the systems with act-key, otherwise it will always fail.(suse-mgr ci specific)
 rhnreg_ks --username=$RHN_USER --password=$RHN_PASS --force \
  --serverUrl=https://$RHN_SERVER/XMLRPC \
- --sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT || show_log count_log
+ --sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT \
+ --activationkey=1-SUSE-PKG-x86_64" || show_log count_log
 
 rhn_check -vv || echo "ERROR: system is not registered"
 
